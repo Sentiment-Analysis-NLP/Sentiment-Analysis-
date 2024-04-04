52dd73e5434ac1cec22e6deae83febb84e652594
AdityaSahu27 <sahuav@rknec.edu>
2024-04-04 08:31:09 +0000

    redeploy

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from preprocessing import *
 
 
 if __name__ == '__main__':
-  setup()
-  print("Getting data...")
-  data, dataset = Scrape()
-  print("Data Acquired Successfully...")
-  print("Generating sentiment analysis...")
-  output = get_sentiment(data)
-  print("Sentiment analysis complete...")
+  # setup()
+  # print("Getting data...")
+  # data, dataset = Scrape()p
+  # print("Data Acquired Successfully...")
+  # print("Generating sentiment analysis...")
+  # output = get_sentiment(data)
+  # print("Sentiment analysis complete...")
   Gui()
 
 # Read CSV file
